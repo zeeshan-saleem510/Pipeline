13020bc08555312a2cc9c9fca952f9a0c1aa6228
zeeshan-saleem510 <zeeshan.saleem510@gmail.com>
2025-08-06 11:52:23 +0100

    RG code

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+  subscription_id = "805cc58a-b2c1-4e91-97ec-b0ba35c3dc96" # Replace with your subscription ID
+}
+
+resource "azurerm_resource_group" "RG_Zee_Terraform" {
+  name     = "AA_RG_Zee_Terraform"
+  location = "East US"
+}
